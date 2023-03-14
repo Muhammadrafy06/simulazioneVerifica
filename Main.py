94bfde44285b6886b562c419fcf6a85c9f1c76a8
Muhammadrafy06 <muhammad.abdul@iisgalvanimi.edu.it>
2023-03-14 11:16:04 +0000

    partial map added

--- Main.py
+++ Main.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def hello_world():
     time = datetime.datetime.now()
     return render_template("index.html", dt=time)
+@app.route('/700', methods=['GET'])
+def size_1():
+  return render_template("index1.html")
 
 
 if __name__ == '__main__':
