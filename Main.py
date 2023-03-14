796a1e382bb43bf0a4a5af00e5e0d937f68d1ada
Muhammadrafy06 <muhammad.abdul@iisgalvanimi.edu.it>
2023-03-14 10:47:56 +0000

    map embedded

--- Main.py
+++ Main.py
@@ -1,9 +1,12 @@
 from flask import Flask,render_template
+import datetime
+
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello_world():
-    return render_template("index.html")
+    time = datetime.datetime.now()
+    return render_template("index.html", dt=time)
 
 
 if __name__ == '__main__':
