690c5ed35fb8ea6d820e22065a85c3b8f7586c3b
Muhammadrafy06 <muhammad.abdul@iisgalvanimi.edu.it>
2023-03-14 11:24:04 +0000

    map size added

--- Main.py
+++ Main.py
@@ -7,10 +7,22 @@ app = Flask(__name__)
 def hello_world():
     time = datetime.datetime.now()
     return render_template("index.html", dt=time)
-@app.route('/700', methods=['GET'])
+
+@app.route('/600', methods=['GET'])
 def size_1():
-  return render_template("index1.html")
+  time = datetime.datetime.now()
+  return render_template("index1.html", dt=time)
+
+@app.route('/800', methods=['GET'])
+def size_2():
+  time = datetime.datetime.now()
+  return render_template("index2.html", dt=time)
+
+@app.route('/1000', methods=['GET'])
+def size_3():
+  time = datetime.datetime.now()
+  return render_template("index3.html", dt=time)
 
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080, debug=True)
+  app.run(host='0.0.0.0', port=3245, debug=True)
